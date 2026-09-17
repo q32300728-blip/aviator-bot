20b398ab7df16d618047f9ca55b6d21721108770
q32300728-blip <q32300728@gmail.com>
2026-09-17 19:03:43 +0600

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from bot import listen_game
-
-if __name__ == "__main__":
-    print("Starting Aviator Signal Bot...")
-    try:
-        asyncio.run(listen_game())
-    except KeyboardInterrupt:
-        print("\nBot stopped by user.")
